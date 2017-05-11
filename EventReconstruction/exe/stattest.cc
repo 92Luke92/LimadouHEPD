10136e80058caa3f0ce78a8222b7dc9a215bf131
lb <Laurent Basara>
2017-05-11 18:09:46 +0200

    Added moments of all order

--- EventReconstruction/exe/stattest.cc
+++ EventReconstruction/exe/stattest.cc
@@ -8,15 +8,15 @@ using namespace std;
 int main (int argc, char *argv[])
 {
 
-    std::vector<int> data={2, 3, 4, 5, 2, 3, 3, 3};
+    std::vector<int> data={1, 2, 3, 2, 1, 3, 3, 2, 2};
     LStatTools stat(data);
     stat.dump();
     LStatTools stat2({360, 362, 364, 325}, {5, 2, 3, 4});
     std::cout << stat.mean() << " -- " << stat.rms()<< std::endl;
-    std::cout << " --- " << std::endl;
+
     stat2.dump();
-    std::cout << " --- " << std::endl;
-    std::cout << stat2.mean() << " -- " << stat2.rms()<< std::endl;
+
+
     return 0;
 }
 
