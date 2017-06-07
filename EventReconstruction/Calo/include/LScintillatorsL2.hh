5e87741db170445c5e4a350dae1c17eee1b82c59
vitalevitncenzo <vincenzo.vitale@roma2.infn.it>
2017-06-07 14:43:42 +0200

    LScintillatorL2 + VVtest

---
+++ EventReconstruction/Calo/include/LScintillatorsL2.hh
@@ -0,0 +1,36 @@
+#ifndef __LScintillatorsL2__
+#define __LScintillatorsL2__ "LScintillatorsL2  ########## "
+#include "LEvRec1.hh"
+#include "LEvRec1File.hh"
+
+class LScintillatorsL2 {
+
+private:
+  LScintillatorsL2();
+  ~LScintillatorsL2();
+
+ 
+
+  LEvRec1File* RunFile;
+ 
+
+  
+ 
+
+ public:
+  static LScintillatorsL2& GetInstance();
+  int LoadRun(const std::string inputFileROOT);
+  
+  int Test();
+
+  LScintillatorsL2(LScintillatorsL2 const&) = delete;
+  void operator=(LScintillatorsL2 const&) = delete;
+
+  
+
+};
+
+
+
+
+#endif
