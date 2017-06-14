088bde1e57d1cd23e873feb1c951079e24d7546e
lb <Laurent Basara>
2017-06-14 19:49:52 +0200

    Corrected warnings -Wunused-variable and -Wsign-compare in gmm.cc and LStatTools.cc

--- EventReconstruction/Common/src/gmm.cc
+++ EventReconstruction/Common/src/gmm.cc
@@ -51,7 +51,7 @@ void GMM::EMstep () {
    // Expectation
    std::array<std::vector<float>,ngaus> w_vals;
 
-   for (int ib=0; ib<bins.size(); ib++) {
+   for (uint ib=0; ib<bins.size(); ib++) {
       float tot_prob=0;
       std::vector<float> prob;
       for (int ig=0; ig<ngaus; ig++) {
