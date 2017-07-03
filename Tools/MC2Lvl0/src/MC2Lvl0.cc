110f9d4268c1f3c5715f716cbba284dc5fa748d9
Laurent-Cnaf <laurent.basara@tifpa.infn.it>
2017-07-03 13:48:30 +0200

    Added loader for MC file; loads with error messages

--- Tools/MC2Lvl0/src/MC2Lvl0.cc
+++ Tools/MC2Lvl0/src/MC2Lvl0.cc
@@ -4,6 +4,7 @@
 
 // C++ std
 #include <iostream>
+#include <string>
 
 // MCEventAnalyze
 #include "RootEvent.hh"
@@ -15,6 +16,9 @@
 // ROOT libs
 #include "TTree.h"
 #include "TVector3.h"
+#include "TTree.h"
+#include "TFile.h"
+#include "TDirectory.h"
 
 //Various
 #include "LEvRec0.hh"
@@ -26,9 +30,36 @@ float Vector3Dist(TVector3 v1, TVector3 v2)
 
 }
 
-//void InitTreeT(TTree* T);
 
-int main() {
+
+
+std::string  getMCfilename(int argc, char** argv) {
+   std::string filename="/storage/gpfs_data/limadou/mpuel/HEPDSW_had_v2/run/Simulations_root/hepd5000_qmd_173MeV_proton_3C0.root";
+   if (argc>1) filename=argv[1];
+   std::cout << "MC2Lvl0: MC file name set to " << filename << std::endl;
+   return filename;
+
+}
+
+std::string  getLvl0filename(const std::string mcfilename) {
+   std::string lvl0filename="test.root";
+   return lvl0filename;
+}
+
+
+
+int main(int argc, char** argv) {
+
+   const std::string mcfilename=getMCfilename(argc, argv);
+   const std::string lvl0filename=getLvl0filename(mcfilename);
+
+   TFile* filemc= TFile::Open(mcfilename.c_str(), "READ");
+   TDirectoryFile* dir = (TDirectoryFile*) filemc->Get("HEPD");
+   TTree* Tmc = (TTree*)dir->Get("EventTree");
+   std::cout << Tmc->GetEntries() << std::endl;
+
+   TFile* filelvl0= TFile::Open(lvl0filename.c_str(), "RECREATE");
+   TTree* Tlvl0 = new TTree("T", "T");
 
    LEvRec0 evtstruc;
    evtstruc.DumpEventIndex();
@@ -36,16 +67,13 @@ int main() {
    std::cout << " >>> ELOSS 3" << hit.GetELoss() << std::endl;
    return 0;
 
+   TTree T("T", "T");
    //TTree outtree=GetTTreeT();
 
 }
 
 
-//TTree GetTTreeT() {return TTree::TTree("T", "T");}
 
 
-/*class LVL0Tree () {
 
-
-};*/
 
