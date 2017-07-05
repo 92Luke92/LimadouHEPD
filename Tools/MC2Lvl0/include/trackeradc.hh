4d79b30e8c89415e51add9cc3038200d90960ffc
Laurent-Cnaf <laurent.basara@tifpa.infn.it>
2017-07-05 15:47:36 +0200

    Added file + main method

---
+++ Tools/MC2Lvl0/include/trackeradc.hh
@@ -0,0 +1,34 @@
+/*
+ * trackeradc.hh
+ *
+ * Tracker Edep to ADC
+ *
+ */
+
+
+#ifndef TRACKERADC_HH
+#define TRACKERADC_HH
+
+#include <vector>
+#include "PMTinfo.h"
+#include "detector_const.hh"
+
+class TrackerADC
+{
+   public:
+      TrackerADC( std::vector<std::vector<PMTinfo>> vvinfo) : PMTinfos(vvinfo) {};
+      std::vector<short> GetStrips();
+
+
+   private:
+     enum trSides {p0, n0, p1, n1, p2, n2, p3, n3, p4, n4, p5, n5 };
+     // Next line : see https://stackoverflow.com/questions/8498300/allow-for-range-based-for-with-enum-classes
+     const std::array<trSides, 12> trSidesIterator=   {trSides::p0, trSides::n0, trSides::p1, trSides::n1, trSides::p2, trSides::n2, trSides::p3, trSides::n3, trSides::p4, trSides::n4, trSides::p5, trSides::n5};
+     std::vector<std::vector<PMTinfo>> PMTinfos;
+     float Mev2ADCFactor(trSides side);
+     std::vector<short> getStripsForSide(trSides side);
+     short TrimADC (float raw, float ped);
+
+};
+
+#endif /* TRACKERADC_HH */
---
+++ Tools/MC2Lvl0/src/trackeradc.cc
@@ -0,0 +1,69 @@
+/*
+ * trackeradc.cc
+ *
+ *
+ *
+ */
+
+
+#include "trackeradc.hh"
+
+
+
+
+std::vector<short> TrackerADC::GetStrips() {
+   std::vector<short> allStrips(NCHAN);
+   for (trSides side : trSidesIterator) {
+      uint offset = side*SIDE_CHAN;
+      std::vector<short> sideStrips=getStripsForSide(side);
+      for (uint is=0; is<SIDE_CHAN; is++)
+         allStrips[offset+is]=sideStrips[is];
+   }
+   return allStrips;
+}
+
+
+float TrackerADC::Mev2ADCFactor (trSides side)
+{
+    float factor = 400;
+    switch (side) {
+       case p0: factor=343; break; // avg p2, p3
+       case n0: factor=425; break;// n2
+       case p1: factor=343; break;
+       case n1: factor=425; break;
+       case p2: factor=341.667; break;
+       case n2: factor=425.333; break;
+       case p3: factor=345.33; break;
+       case n3: factor=0; break; // shit ain't working
+       case p4: factor=343; break;
+       case n4: factor=425; break;
+       case p5: factor=343; break;
+       case n5: factor=425; break;
+    }
+    return factor;
+}
+
+
+
+std::vector<short> TrackerADC::getStripsForSide (trSides side) {
+   std::vector<short> sideStrips (SIDE_CHAN);
+   float Mev2ADCfactor=Mev2ADCFactor(side);
+   float totEdep=0;
+   std::vector<PMTinfo> SidePMTinfos = PMTinfos[side];
+   for (auto chaninfo: SidePMTinfos) totEdep+=chaninfo.totEdep;
+   float averagedEdep=totEdep / float(SIDE_CHAN);
+   float averageADC= averagedEdep*Mev2ADCfactor;
+   float dummypedvalue=100;
+   short chanADC=TrimADC(averageADC, dummypedvalue);
+   for (auto strip : sideStrips) strip=chanADC;
+   return sideStrips;
+
+}
+
+short TrackerADC::TrimADC (float raw, float ped)
+{
+    int untrimmed = static_cast<int> (raw + ped);
+    if (untrimmed > NADC) untrimmed = NADC - 1;
+    short trimmed=static_cast<short> (untrimmed);
+    return trimmed;
+}
