2943c5686169463453d49ec9096205556034fa65
LimadouHEPD <folly.92@hotmail.it>
2017-12-01 11:16:01 +0100

    skim event modified

--- EventReconstruction/exe/LSkimmingEvents.cc
+++ EventReconstruction/exe/LSkimmingEvents.cc
@@ -11,39 +11,42 @@
 int main(int argc, char **argv) {
   if(argc!=4) {
     std::cerr << "************ Usage:" << std::endl
-	      << "************ ~user> ./LSkimmingEvents.cc <inputDir> <inputPath> <outputFile>" << std::endl
+	      << "************ ~user> ./LSkimmingEvents <inputDir> <inputPath> <outputFile>" << std::endl
 	      << "************ aborting."
 	      << std::endl;
     return -999;
   }
   //define the desired offline trigger
   LOfflineTrigger mytrig;
-  mytrig.LoadConfiguration("central_muon_lyso4");
+  mytrig.LoadConfiguration("central_muon");
   LOfflineTrigger veto;
   veto.LoadConfiguration("VL");
-  //mytrig.LoadConfiguration("central_muon_tight");
   //define output file
   LEvRec1File outputFile(argv[3],"WRITE");
   LEvRec1 ev; 
   //change directory to the target one
   TSystemDirectory dir(argv[1], argv[2]);
   TList *files = dir.GetListOfFiles();
+  
   if(files) {
     TSystemFile *file;
     TIter next(files);
     TString fname;
     std::string point = ".";
     std::string pointpoint = "..";
+    std::string DS = ".DS_Store";
     int counter = 0;
     int counter2 = 0;
     while ((file=(TSystemFile*)next())) {
       counter++;
-      //if (counter>100) break; 
       fname = file->GetName();
-      if(fname == point.c_str() || fname == pointpoint.c_str()) continue;
+      std::cout<< file->GetName() << std::endl;
+      if(fname == point.c_str() || fname == pointpoint.c_str() || fname == DS.c_str() ) continue;
+      std::cout<< "cavolo" << std::endl;
       TString ffname = TString(argv[2])+"/"+fname;
       std::cout << "********* Processing file " << ffname << std::endl;
       LEvRec1File inputFile(ffname.Data(),"READ");
+      std::cout << "********* Processing file " << ffname << std::endl;
       inputFile.SetTheEventPointer(ev);
       int nentries=inputFile.GetEntries();
       for(int ie=0; ie<nentries; ++ie) { 
