76a2e02d91840e0176b1baa2f8980a4a95fc4874
EsterRicci <ester.ricci@unitn.it>
2018-03-14 09:31:23 +0100

    Tracker QL for compressed mode

--- QuickLook/include/LTrackerQuicklook.hh
+++ QuickLook/include/LTrackerQuicklook.hh
@@ -2,9 +2,11 @@
 #define LTRACKERQUICKLOOK 1
 
 #include "detector_const.hh"
+#include "LEvRec0File.hh"
 
 #include "TCanvas.h"
 #include "TH2.h"
+#include "TString.h"
 
 const double HOTCHANNELTHRESHOLD=20.;
 const double COLDCHANNELTHRESHOLD=3.;
@@ -15,6 +17,8 @@ const int NCALIBEVENTS_QL=1024;
 int TrackerQuickLook(TString namefile, TString outPath);
 void LTrackerCalibrationQL(std::string namefile);
 void PrintHelp(void);
+
+int CompressedTrackerQL(LEvRec0File input,TString outname);
 
 #endif
  
