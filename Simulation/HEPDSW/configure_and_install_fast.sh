21bf3be7a957bd3e02477c5ef5faa3217085dd09
Luke <luca.carfora.2@hotmail.it>
2018-07-23 11:40:10 +0200

    Changed name of HEPDSW folder

--- Simulation/HEPDSW/configure_and_install_fast.sh
+++
@@ -1,9 +0,0 @@
-hepd_dir=$PWD
-mkdir build
-cd build
-echo "Configuration  with cmake!"
-cmake -DSTATIC_BUILD=OFF -DHEPDSW_INSTALL_PREFIX=$hepd_dir $hepd_dir
-echo "Compilation of the code with make!"
-make -j4
-echo "Installation of the code!"
-make install
