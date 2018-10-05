35bff0366c530ce39666fde82619f7bc192a359c
Luke <luca.carfora.2@hotmail.it>
2018-10-05 14:08:13 +0200

    some changes to multiscript args

--- Simulation/HEPDSW_v4/run/multi_jobs.sh
+++ Simulation/HEPDSW_v4/run/multi_jobs.sh
@@ -1,14 +1,16 @@
-#for instance, FILENAME = p_125MeV_3C
-FILENAME=$1
 
-for i in `seq 1 3`;
+FILENAME=$1
+NJOBS=$2
+NEVENTS=$3
+
+for i in `seq 1 $NJOBS`;
 do
     MACFILE=$FILENAME"_"$i".mac"
     MACPATH="job/"$MACFILE
     cp base.mac $MACPATH
     echo "/random/setSeeds $((1 + RANDOM % 1000000)) $((1 + RANDOM % 1000000))" >> $MACPATH
     echo "/hepd/producer/SetRootFile "$FILENAME"_"$i".root HEPD" >> $MACPATH
-    echo "/run/beamOn 1" >> $MACPATH
+    echo "/run/beamOn "$NEVENTS >> $MACPATH
     echo "/hepd/producer/StoreRootFile" >> $MACPATH
 
     bsub -q limadou -e err$i/ -o log$i/ hepdsw $MACPATH
