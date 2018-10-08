5487d17810d3b5aef3c1b762b90594f38b7dff97
Luke <luca.carfora.2@hotmail.it>
2018-10-08 11:57:59 +0200

    modified multi_jobs

--- Simulation/HEPDSW_v4/run/multi_jobs.sh
+++ Simulation/HEPDSW_v4/run/multi_jobs.sh
@@ -2,12 +2,15 @@
 FILENAME=$1
 NJOBS=$2
 NEVENTS=$3
+RANDOMVAR=$4
 
 for i in `seq 1 $NJOBS`;
 do
+
     MACFILE=$FILENAME"_"$i".mac"
     MACPATH="job/"$MACFILE
     cp base.mac $MACPATH
+    RANDOM=$RANDOMVAR
     echo "/random/setSeeds $((1 + RANDOM % 1000000)) $((1 + RANDOM % 1000000))" >> $MACPATH
     echo "/hepd/producer/SetRootFile "$FILENAME"_"$i".root HEPD" >> $MACPATH
     echo "/run/beamOn "$NEVENTS >> $MACPATH
